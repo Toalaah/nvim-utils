b61387a049668c64f85a908a5c5706fba01659ec
Samuel Kunst <samuel@kunst.me>
2023-03-12 13:11:00 +0100

    chore: fix doc wording, clean up assertion type checker

--- lib/assertions.nix
+++ lib/assertions.nix
@@ -1,14 +1,16 @@
 /*
-Defines the assertion type. Allows for checking module integrity via custom
-validators
+Defines the assertion option type. Enabled checking of module integrity via
+custom validators / assertions.
 */
 {lib}:
 with lib; let
   hasAttrOfType = attr: type: x:
-    (builtins.hasAttr attr x) && (builtins.typeOf x.${attr} == type.name);
+    builtins.hasAttr attr x && type.check x.${attr};
   assertionType = types.mkOptionType {
     name = "assertion";
-    check = x: (hasAttrOfType "assertion" types.bool x) && (hasAttrOfType "message" lib.types.string x);
+    check = x:
+      hasAttrOfType "assertion" types.bool x
+      && hasAttrOfType "message" types.str x;
   };
 in {
   options = {
