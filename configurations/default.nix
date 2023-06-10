39bc6387fc9c566481f5e8f845bdd7e57f05da99
Samuel Kunst <samuel@kunst.me>
2023-06-10 18:01:25 +0200

    update example configuration

--- configurations/default.nix
+++ configurations/default.nix
@@ -74,64 +74,58 @@ rec {
     #   g.mapleader = " ";
     # };
 
+    vim.opt.backup = false;
+    vim.opt.undofile = true;
+    vim.opt.clipboard = "unnamedplus";
+    vim.opt.cmdheight = 0;
+    vim.opt.completeopt = "menu,menuone,noselect";
+    # vim.cmd [[set spelllang=en_us,de_de ]];
+    vim.opt.confirm = true;
+    vim.opt.cursorline = true;
+    vim.opt.diffopt = "vertical";
+    # vim.o.foldcolumn = "1";
+    # vim.o.foldlevel = 99;
+    # vim.o.foldlevelstart = 99;
+    # vim.o.foldenable = true;
+    vim.opt.splitright = true;
+    vim.opt.splitbelow = true;
+    vim.opt.expandtab = true;
+    vim.opt.hidden = true;
+    vim.opt.ignorecase = true;
+    vim.opt.inccommand = "nosplit";
+    vim.opt.incsearch = true;
+    vim.opt.laststatus = 0;
+    vim.opt.list = true;
+    vim.opt.listchars = {
+      extends = "⟩";
+      nbsp = "␣";
+      precedes = "⟨";
+      tab = "→\\\\ ";
+      trail = "•";
+      # eol = "↴";
+      # space = "⋅";
+    };
+    vim.opt.mouse = "a";
+    vim.opt.number = true;
+    vim.opt.relativenumber = true;
+    vim.opt.scrolloff = 8;
+    vim.opt.shiftwidth = 2;
+    vim.opt.showbreak = "↪";
+    vim.opt.showmode = false;
+    vim.opt.showtabline = 0;
+    vim.opt.signcolumn = "auto:2";
+    vim.opt.smartcase = true;
+    vim.opt.swapfile = false;
+    vim.opt.tabstop = 2;
+    vim.opt.termguicolors = true;
+    vim.opt.timeoutlen = 250;
+    vim.opt.wrap = false;
 
-
-
-vim.opt.backup = false;
-vim.opt.undofile = true;
-vim.opt.clipboard = "unnamedplus";
-vim.opt.cmdheight = 0;
-vim.opt.completeopt = "menu,menuone,noselect";
-# vim.cmd [[set spelllang=en_us,de_de ]];
-vim.opt.confirm = true;
-vim.opt.cursorline = true;
-vim.opt.diffopt = "vertical";
-# vim.o.foldcolumn = "1";
-# vim.o.foldlevel = 99;
-# vim.o.foldlevelstart = 99;
-# vim.o.foldenable = true;
-vim.opt.splitright = true;
-vim.opt.splitbelow = true;
-vim.opt.expandtab = true;
-vim.opt.hidden = true;
-vim.opt.ignorecase = true;
-vim.opt.inccommand = "nosplit";
-vim.opt.incsearch = true;
-vim.opt.laststatus = 0;
-vim.opt.list = true;
-vim.opt.listchars = {
-  extends = "⟩";
-  nbsp = "␣";
-  precedes = "⟨";
-  tab = "→\\\\ ";
-  trail = "•";
-  # eol = "↴";
-  # space = "⋅";
-};
-vim.opt.mouse = "a";
-vim.opt.number = true;
-vim.opt.relativenumber = true;
-vim.opt.scrolloff = 8;
-vim.opt.shiftwidth = 2;
-vim.opt.showbreak = "↪";
-vim.opt.showmode = false;
-vim.opt.showtabline = 0;
-vim.opt.signcolumn = "auto:2";
-vim.opt.smartcase = true;
-vim.opt.swapfile = false;
-vim.opt.tabstop = 2;
-vim.opt.termguicolors = true;
-vim.opt.timeoutlen = 250;
-vim.opt.wrap = false;
-
-vim.g.tex_flavor = "latex";
-vim.g.mapleader = " ";
-vim.g.editorconfig = true;
-
-
+    vim.g.tex_flavor = "latex";
+    vim.g.mapleader = " ";
+    vim.g.editorconfig = true;
   };
 
   # TODO: multiple configs for different workflows (programming / prose / devops?)
   minimal = default;
 }
-
