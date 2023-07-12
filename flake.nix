517f9378be450e7ab14c5ad230282b7adbcbc804
Samuel Kunst <samuel@kunst.me>
2023-07-12 19:02:56 +0200

    remove dependency on flake-utils

--- flake.nix
+++ flake.nix
@@ -1,48 +1,47 @@
 {
   description = "Utilities for creating extensible, reproducible, and portable lazy.nvim-based neovim configurations in nix";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-
-    neovim-nightly.url = "github:neovim/neovim?dir=contrib";
-    neovim-nightly.inputs.nixpkgs.follows = "nixpkgs";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
   outputs = {
     self,
-    flake-utils,
-    neovim-nightly,
     nixpkgs,
     ...
-  } @ inputs: let
+  }: let
     mkModule = import ./module.nix;
-  in
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-      nvimPkg = import ./package;
-      configurations = import ./configurations;
-      neovim-nightly' = inputs.neovim-nightly.packages.${system}.neovim;
-    in {
-      # map each configuration to an individual package
-      packages = builtins.mapAttrs (name: _:
-        pkgs.callPackage nvimPkg {
-          configuration = configurations.${name};
-          package = neovim-nightly';
-        })
-      configurations;
+    eachSystem = f:
+      nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ] (system: let pkgs = import nixpkgs {inherit system;}; in f {inherit system pkgs;});
+  in {
+    homeManagerModules.nvim = mkModule {};
+    homeManagerModules.default = self.homeManagerModules.nvim;
 
-      formatter = pkgs.alejandra;
+    nixosModules.nvim = mkModule {nixos = true;};
+    nixosModules.default = self.nixosModules.nvim;
 
-      devShells.default = import ./shell.nix {inherit pkgs;};
-    })
-    // {
-      homeManagerModules.nvim = mkModule {};
-      homeManagerModules.default = self.homeManagerModules.nvim;
+    lib.baseModules = ./modules;
 
-      nixosModules.nvim = mkModule {nixos = true;};
-      nixosModules.default = self.nixosModles.default;
+    formatter = eachSystem ({pkgs, ...}: pkgs.alejandra);
 
-      lib.baseModules = ./modules;
-    };
+    devShells = eachSystem ({pkgs, ...}: {
+      default = import ./shell.nix {inherit pkgs;};
+    });
+
+    packages = eachSystem (
+      {pkgs, ...}: let
+        mkNvimPkg = pkgs.callPackage (import ./package);
+        configurations = import ./configurations;
+      in
+        builtins.mapAttrs (name: _:
+          mkNvimPkg {
+            configuration = configurations.${name};
+            modules = [self.lib.baseModules];
+          })
+        configurations
+    );
+  };
 }
