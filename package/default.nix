ebacbcb3bed53b1fcc08108f6095f047699389c9
Samuel Kunst <samuel@kunst.me>
2023-07-15 18:44:36 +0200

    use init.lua instead of init.vim

--- package/default.nix
+++ package/default.nix
@@ -11,22 +11,21 @@
     inherit configuration lazy-nvim;
     modules' = {imports = modules;};
   };
-  wrapLuaConfig = luaCode: ''
-    lua << EOF
-    ${luaCode}
-    EOF
-  '';
-in
-  pkgs.wrapNeovim package {
-    extraMakeWrapperArgs = lib.strings.concatStringsSep " " [
-      "--prefix PATH : ${cfg.extraPkgs}/bin"
-      ''--add-flags "--cmd 'set rtp^=${lazy-nvim},${cfg.rtp}'"''
-    ];
-    configure.customRC = wrapLuaConfig ''
+  init_lua = pkgs.writeTextFile {
+    name = "init.lua";
+    text = ''
       ${cfg.preHooks}
       require('lazy').setup(${cfg.plugins}, ${cfg.lazy})
       ${cfg.vim.opt}
       ${cfg.vim.g}
       ${cfg.postHooks}
     '';
+  };
+in
+  pkgs.wrapNeovim package {
+    extraMakeWrapperArgs = lib.strings.concatStringsSep " " [
+      "--prefix PATH : ${cfg.extraPkgs}/bin"
+      ''--add-flags "--cmd 'set rtp^=${lazy-nvim},${cfg.rtp}'"''
+      "--add-flags '-u ${init_lua}'"
+    ];
   }
