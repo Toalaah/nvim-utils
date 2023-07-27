277d36b854ed60086bd71f5a01120801eb7ad3c7
Samuel Kunst <samuel@kunst.me>
2023-07-27 17:51:24 +0200

    add remaining autocmd opts, refactor module

--- modules/core/autocmd.nix
+++ modules/core/autocmd.nix
@@ -5,44 +5,80 @@
   ...
 }:
 with lib; let
-  stringifyAutoCmd = au: ''
-    vim.api.nvim_create_autocmd(${toLua au.event}, {
-      pattern = ${toLua au.pattern},
-      callback = ${toLua au.callback},
-      group = ${toLua au.group},
-      ${optionalString (au.description != null) "desc = ${toLua au.desc},"}
-    })
-  '';
+  stringifyAutoCmd = let
+    ifNotNull = value: field: lib.optionalString (value != null) "${field} = ${toLua value},";
+    ifNotFalse = value: field: lib.optionalString value "${field} = ${toLua value},";
+  in
+    au: ''
+      vim.api.nvim_create_autocmd(${toLua au.event}, {
+        pattern = ${toLua au.pattern},
+        ${ifNotNull au.callback "callback"}
+        ${ifNotNull au.command "command"}
+        ${ifNotNull au.group "group"}
+        ${ifNotNull au.description "desc"}
+        ${ifNotFalse au.once "once"}
+        ${ifNotFalse au.nested "nested"}
+      })
+    '';
   inherit (config) autocmds;
-  optionalStrList = types.oneOf [(types.listOf types.str) types.str];
+  listOrSingletonOf = t: types.oneOf [(types.listOf t) t];
   strOrLuaFunction = types.oneOf [(types.functionTo types.str) types.str];
 in {
   options.autocmds = mkOption {
     type = types.listOf (types.submodule {
       options = {
         event = mkOption {
-          type = optionalStrList;
+          type = listOrSingletonOf strOrLuaFunction;
         };
         pattern = mkOption {
-          type = optionalStrList;
+          type = listOrSingletonOf strOrLuaFunction;
         };
         description = mkOption {
           type = types.nullOr types.str;
           default = null;
         };
         group = mkOption {
-          type = strOrLuaFunction;
+          type = types.nullOr strOrLuaFunction;
+          default = null;
         };
-        # to be called w/ rawLua
+        # can be called w/ rawLua (if lua function is desired, else if string
+        # callback is interpreted as vimscript fn)
         callback = mkOption {
-          type = strOrLuaFunction;
+          type = types.nullOr strOrLuaFunction;
+          default = null;
+        };
+        command = mkOption {
+          type = types.nullOr types.str;
+          default = null;
+        };
+        once = mkOption {
+          type = types.boolean;
+          default = false;
+        };
+        nested = mkOption {
+          type = types.boolean;
+          default = false;
         };
       };
     });
     default = [];
-    description = "list of autocmds to set";
+    description = lib.mdDoc ''
+      List of autocmds to set. Refer to `vim.api.nvim_create_autocmd` for usage
+      and option documentation.
+    '';
   };
   config = {
+    assertions = let
+      xor = a: b: (a == null) != (b == null);
+      invalidAuCmds = lib.lists.filter (x: !(xor x.callback x.command)) autocmds;
+    in
+      builtins.map (
+        x: {
+          assertion = false;
+          message = "autocmd: exactly one of `command` or `callback` must be specified";
+        }
+      )
+      invalidAuCmds;
     postHooks = ''
       ${lib.strings.concatStringsSep "\n" (builtins.map stringifyAutoCmd autocmds)}
     '';
