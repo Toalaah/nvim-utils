1d0207ba12aecf9b7279bf2cfb2514272e5e0ac6
Samuel Kunst <samuel@kunst.me>
2023-08-08 11:25:08 +0200

    chore: pin pkgs rev for docs drv be default

--- docs/build.nix
+++ docs/build.nix
@@ -1,18 +1,10 @@
-{
-  mdbook,
-  mdbook-cmdrun,
-  nixdoc,
-  pkgs,
-  stdenv,
-  writeShellScriptBin,
-  ...
-}: let
-  tmpPkgs = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/c2596b396ca0a0d9f82fa6e64839b37473008a58.tar.gz") {};
+{pkgs ? (import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/c2596b396ca0a0d9f82fa6e64839b37473008a58.tar.gz") {})}: let
+  inherit (pkgs) mdbook mdbook-cmdrun nixdoc stdenv writeShellScriptBin;
   nixdoc-to-md = writeShellScriptBin "nixdoc-to-md.sh" (builtins.readFile ./nixdoc-to-md.sh);
   module-docs = import ./build-module-docs.nix {inherit pkgs;};
   options-to-md = writeShellScriptBin "options-to-md.sh" ''
     [ -z "$1" ] && echo "Usage: $0 <module>" >&2  && exit 1
-    cat ${module-docs}/$1/out.md
+    cat ${module-docs}/$1.md
   '';
 in
   stdenv.mkDerivation {
@@ -25,7 +17,7 @@ in
     buildInputs = [
       mdbook
       mdbook-cmdrun
-      tmpPkgs.nixdoc
+      nixdoc
       nixdoc-to-md
       options-to-md
     ];
