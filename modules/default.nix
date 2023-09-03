0106240d2676dfb0239f6791a00132b4f674d749
Samuel Kunst <samuel@kunst.me>
2023-09-03 12:14:07 +0200

    remove non-core modules

--- modules/default.nix
+++ modules/default.nix
@@ -1,11 +1,6 @@
 {
   core = ./core;
 
-  colorschemes.tokyonight = ./colorschemes/tokyonight;
-  colorschemes.rose-pine = ./colorschemes/rose-pine;
-
-  git.gitsigns = ./git/gitsigns;
-
   languages.lua = ./languages/lua;
   languages.nix = ./languages/nix;
   languages.rust = ./languages/rust;
@@ -13,8 +8,6 @@
   lazy = ./lazy;
 
   lsp = ./lsp;
-
-  tools.zk = ./tools/zk;
 
   telescope = ./telescope;
 
